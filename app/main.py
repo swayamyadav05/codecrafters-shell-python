76bbe97362fec6e41300bf89de02d0ee4738418f
Mangloo69 <yadav69.swayam@gmail.com>
2025-02-01 17:45:14 +0545

    returning status 0

--- app/main.py
+++ app/main.py
@@ -5,12 +5,13 @@ def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
 
-    # Wait for user input
-    command = input()
-    if command == "exit 0":
-        return
-    print(f"{command}: command not found")
-    main()
+    while True:
+        # Wait for user input
+        command = input()
+        if command == "exit 0":
+            return 0
+        print(f"{command}: command not found")
+        main()
 
 
 if __name__ == "__main__":
