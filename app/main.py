effed1a6a9db09c69ef1dab2dc9f482bb67d84a1
Mangloo69 <yadav69.swayam@gmail.com>
2025-02-01 19:02:49 +0545

    type builtin

--- app/main.py
+++ app/main.py
@@ -3,21 +3,33 @@ import sys
 
 def main():
     while True:
+        # Display the prompt
         sys.stdout.write("$ ")
+        sys.stdout.flush()
 
+        shell_builtin = ["exit", "echo", "type"]
         # Wait for user input
         command = input().strip()
 
+        # Skip empty commands
         if not command:
             continue
 
+        # Check for exit condition
         if command == "exit 0":
             sys.exit()
 
+        # Split the command into words
         cmd = command.split()
 
+        # Process the command using and if-elif-else structure
         if cmd[0] == "echo":
+            # Print everything after the command name
             print(" ".join(cmd[1:]))
+        elif cmd[0] == "type" and cmd[1] in shell_builtin:
+            print(f"{cmd[1]} is a shell builtin")
+        elif cmd[0] == "type" and cmd[1] not in shell_builtin:
+            print(f"{cmd[1]}: command not found")
         else:
             print(f"{command}: command not found")
 
